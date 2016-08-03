91a4f1cffda5e3d43c8484fdb109d4503cfeb762
David J. Hamilton <dhamilton@linkedin.com>
2016-08-03 12:06:32 -0700

    More informative failure message on global check

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var compatibleVersion = '^' + version;
 if (process._heimdall) {
   var globalVersion = process._heimdall.version;
   if (!semver.satisfies(globalVersion, compatibleVersion)) {
-    throw new Error('Version "' + globalVersion + '" not compatible with "' + compatibleVersion + '"');
+    throw new Error('Heimdalljs global version "' + globalVersion + '" not compatible with "' + compatibleVersion + '"');
   }
 } else {
   process._heimdall = new Heimdall();
