af127afd075598fb7d597f66fbcda9fb0f556d91
Chris Thoburn <cthoburn@linkedin.com>
2016-08-24 21:02:25 -0700

    benching

--- bench/index.js
+++ bench/index.js
@@ -1,12 +1,63 @@
 // basic runner;
 console.log('running instantiation tests:');
+
+var ITER_COUNT = 1000;
+process.heimdall = require('../');
+
 require('do-you-even-bench')([
   {
-    name: 'node',
-    fn: require('./node')
+    count: ITER_COUNT,
+    name: 'new Cookie()',
+    fn: require('./cookie-new'),
   },
   {
-    name: 'cookie creation',
-    fn: require('./cookie'),
-  }
+    count: ITER_COUNT,
+    name: 'Cookie.create()',
+    fn: require('./cookie-create'),
+  },
+  {
+    count: ITER_COUNT,
+    name: 'Cookie.create() w/prealloc',
+    setup: function() {
+      var heimdall = process.heimdall;
+      var count = this.count;
+      var Cookie = heimdall.constructor.Cookie;
+      var Node = heimdall.constructor.Node;
+      var node = new Node(heimdall, { name: 'some-node' });
+
+      // preallocate
+      while (count--) {
+        var cookie = new Cookie(node, heimdall);
+        cookie._stopped = true;
+        cookie.destroy();
+      }
+    },
+    fn: require('./cookie-create'),
+  },
+  {
+    count: ITER_COUNT,
+    name: 'new Node()',
+    fn: require('./node-new')
+  },
+  {
+    count: ITER_COUNT,
+    name: 'Node.create()',
+    fn: require('./node-create')
+  },
+  {
+    count: ITER_COUNT,
+    name: 'Node.create() w/prealloc',
+    setup: function() {
+      var heimdall = process.heimdall;
+      var Node = heimdall.constructor.Node;
+      var count = this.count;
+
+      // preallocate
+      while (count--) {
+        var node = new Node(heimdall, { name: 'some-node' });
+        node.destroy();
+      }
+    },
+    fn: require('./node-create')
+  },
 ]);
