68bfabe521c031f0c68d079c73f5b1109c58434d
Chris Thoburn <cthoburn@linkedin.com>
2016-08-31 12:17:49 -0700

    faster time marker creation

--- src/time.js
+++ src/time.js
@@ -1,33 +1,39 @@
-let timeNS;
+export let now;
+export let format;
+export let ORIGIN_TIME;
 
-// adapted from
-// https://gist.github.com/paulirish/5438650
-let now;
 if (typeof performance === 'object' && typeof performance.now === 'function') {
-  now = function now() {
-    return performance.now.call(performance);
-  };
+  now = performance.now.bind(performance);
+  format = 'milli';
+} else if (typeof process !== 'undefined' && typeof process.hrtime === 'function') {
+  now = process.hrtime.bind(process);
+  format = 'hrtime';
 } else {
-  const dateOffset = new Date().getTime();
-  now = function now() { return new Date().getTime() - dateOffset; };
+  ORIGIN_TIME = Date.now();
+  now = Date.now.bind(Date);
+  format = 'timestamp';
 }
 
-const dateOffset = now();
-
-export function timeFromDate() {
-  return Math.floor(now() * 1e6);
+export function normalizeTime(time) {
+  switch (format) {
+    case 'milli':
+      return milliToNano(time);
+    case 'hrtime':
+      return timeFromHRTime(time);
+    case 'timestamp':
+      return milliToNano(time - ORIGIN_TIME);
+    default:
+      throw new Error('Unknown Format');
+  }
 }
 
+export function milliToNano(time) {
+  return Math.floor(time * 1e6);
+}
 
 export function timeFromHRTime() {
   let hrtime = process.hrtime();
   return hrtime[0] * 1e9 + hrtime[1];
 }
 
-if (typeof process === 'object' && typeof process.hrtime === 'function') {
-  timeNS = timeFromHRTime;
-} else {
-  timeNS = timeFromDate;
-}
-
-export default timeNS;
+export default now;
