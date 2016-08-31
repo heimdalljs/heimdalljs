5d0df9b4c7b1d50f8a19fcd76f2c8aa1e9401aac
Chris Thoburn <cthoburn@linkedin.com>
2016-08-30 18:35:58 -0700

    less leaky bench

--- bench/index.js
+++ bench/index.js
@@ -1,16 +1,37 @@
-// basic runner;
-console.log('running instantiation tests:');
+var Heimdall = require('../heimdall');
+process.Heimdall = Heimdall;
+
+// var heimdall = require('../');
+// process.heimdall = heimdall;
+
 require('do-you-even-bench')([
   {
     name: 'node',
-    fn: require('./node')
+    count: 1000,
+    fn: require('./node'),
+    setup: function() {
+      var Heimdall = process.Heimdall;
+      var heimdall = new Heimdall();
+      // var heimdall = process.heimdall;
+      var Node = heimdall.constructor.Node;
+    }
   },
   {
+    count: 1000,
     name: 'start',
-    fn: require('./start')
+    fn: require('./start'),
+    setup: function() {
+      var Heimdall = process.Heimdall;
+      var heimdall = new Heimdall();
+    }
   },
   {
+    count: 1000,
     name: 'comprehensive',
-    fn: require('./overall')
+    fn: require('./overall'),
+    setup: function() {
+      var Heimdall = process.Heimdall;
+      var heimdall = new Heimdall();
+    }
   }
 ]);
--- bench/start.js
+++ bench/start.js
@@ -1,13 +1,5 @@
-var heimdall = require('../');
-
-// tests
-module.exports = start;
 function start() {
   let a = heimdall.start('a');
 }
 
-// loop is for quick "hot code" testing
-// while (true) {
-// ensure it works and does't crash
-start();
-// }
+module.exports = start;
