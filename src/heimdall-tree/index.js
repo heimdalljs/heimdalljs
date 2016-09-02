a16f105cbb0290c60fafb06faf0cb2d92880a57a
Chris Thoburn <cthoburn@linkedin.com>
2016-09-02 01:16:37 -0700

    finished initial spike of tree generation

--- src/heimdall-tree/index.js
+++ src/heimdall-tree/index.js
@@ -1,5 +1,6 @@
 // import HeimdallNode from './node';
 import HeimdallLeaf from './leaf';
+import HashMap from '../shared/hash-map';
 import {
   OP_START,
   OP_STOP,
@@ -24,7 +25,7 @@ A
 Leafy Tree
 A <- node
  |_AB <- leaf
- |_ B
+ |_ B <- child node
  | |_ BC
  | |
  | |_C
@@ -37,26 +38,30 @@ A <- node
 */
 
 class HeimdallNode {
-  constructor(name, time) {
+  constructor(name, id, parent) {
+    this._id = id;
+    this.parent = parent;
+    this.resumeNode = parent;
     this.name = name;
-    this.stopTime = null;
-    this.counters = null;
     this.stopped = false;
     this.leaves = [];
     this.nodes = [];
     this.children = [];
   }
 
-  start(time, counters) {
-
+  stop() {
+    if (this.stopped) {
+      throw new Error('Cannot Stop node, already stopped!');
+    }
+    this.stopped = true;
   }
 
-  stop(event) {
-
-  }
-
-  resume() {
-
+  resume(resumeNode) {
+    if (!this.stopped) {
+      throw new Error('Cannot Resume node, already running!');
+    }
+    this.resumeNode = resumeNode;
+    this.stopped = false;
   }
 
   addLeaf(leaf) {
@@ -79,20 +84,23 @@ export default class HeimdallTree {
     this.root = null;
   }
 
-  // heimdall._monitors
-  // heimdall._events
   construct() {
     let events = this._heimdall._events;
-    let roots = [];
     let currentLeaf = null;
-    let root;
-    let currentNode = HeimdallNode('all');
+    let root = new HeimdallNode('---system', 1e9, null);
+    let currentNode = root;
+    let nodeMap = new HashMap();
 
     for (let i = 0; i < events.length; i++) {
       let [op, name, time, counters] = events[i];
 
       switch (op) {
         case OP_START:
+          let node = new HeimdallNode(name, i, currentNode);
+          nodeMap.set(i, node);
+          currentNode.addChild(node);
+          currentNode = node;
+
           if (currentLeaf) {
             currentLeaf.stop(name, time, counters);
           }
@@ -100,13 +108,28 @@ export default class HeimdallTree {
           currentLeaf.start(currentNode, name, time);
           currentNode.addLeaf(currentLeaf);
           break;
+
         case OP_STOP:
+          if (!currentNode) {
+            throw new Error("Cannot Stop, There is no active node!");
+          }
+          if (name !== currentNode._id) {
+            throw new Error("Cannot Stop, Attempting to stop a node with an active child!");
+          }
+          currentNode.stop();
+          currentNode = currentNode.resumeNode;
+
           currentLeaf.stop(name, time, counters);
           currentLeaf = new HeimdallLeaf();
           currentLeaf.start(currentNode, name, time);
           currentNode.addLeaf(currentLeaf);
           break;
+
         case OP_RESUME:
+          let node = nodeMap.get(name);
+          node.resume(currentNode);
+          currentNode = node;
+
           if (currentLeaf) {
             currentLeaf.stop(name, time, counters);
           }
@@ -114,6 +137,7 @@ export default class HeimdallTree {
           currentLeaf.start(currentNode, name, time);
           currentNode.addLeaf(currentLeaf);
           break;
+
         case OP_ANNOTATE:
           currentLeaf.annotate(counters);
           break;
