de2cbde13c21672c36295f6230198a8865f9f18e
Chris Thoburn <cthoburn@linkedin.com>
2016-09-03 12:03:38 -0700

    set testing to true

--- tests/index.js
+++ tests/index.js
@@ -1,3 +1,7 @@
+export const FREE_GLOBAL = typeof window !== 'undefined' ? window : global;
+
+FREE_GLOBAL.IS_HEIMDALL_TEST_ENVIRONMENT = true;
+
 // import './shared-tests';
 import './runtime-tests';
 import './heimdall-tree-tests';
