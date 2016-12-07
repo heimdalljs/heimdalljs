c6f8bced7732c4e9105de8eda83b74064c343c22
Chris Thoburn <cthoburn@linkedin.com>
2016-12-07 15:00:08 -0800

    fix #47 by warning if a global session is already present

--- src/runtime/setup-session.js
+++ src/runtime/setup-session.js
@@ -1,9 +1,12 @@
 import Session from './session';
+import { warn } from '../shared/log';
 
 export default function setupSession(global) {
 
   // The name of the property encodes the session/node compatibilty version
   if (!global._heimdall_session_3) {
     global._heimdall_session_3 = new Session();
+  } else {
+    warn(`Skipped Heimdall Session instantiation as a global session already exists.`);
   }
 }
