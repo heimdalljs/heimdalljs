efa1dfdea50ad37bb0b0deb45966852909d874c2
Chris Thoburn <cthoburn@linkedin.com>
2016-12-16 15:34:58 -0800

    warn if a mismatched global session is present (fix #47)

--- src/runtime/setup-session.js
+++ src/runtime/setup-session.js
@@ -1,9 +1,16 @@
 import Session from './session';
+import { warn } from '../shared/log';
 
 export default function setupSession(global) {
-
   // The name of the property encodes the session/node compatibilty version
   if (!global._heimdall_session_3) {
     global._heimdall_session_3 = new Session();
+  }
+
+  for (let i = 1; i < 3; i++) {
+    let key = `_heimdall_session_${i}`;
+    if (global[key]) {
+      warn(`Using HeimdallSession Version 3, but a global session for HeimdallSession version ${i} also exists!.`);
+    }
   }
 }
