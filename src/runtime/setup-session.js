b28e0426d5ea2c85f22d1cfe25f6b7a6a537e56d
Chris Thoburn <cthoburn@linkedin.com>
2016-12-16 15:43:18 -0800

    make it a little more obvious what the magic numbers are

--- src/runtime/setup-session.js
+++ src/runtime/setup-session.js
@@ -1,16 +1,20 @@
 import Session from './session';
 import { warn } from '../shared/log';
 
+const CURRENT_VERSION = 3;
+const SESSION_KEY_PREFIX = '_heimdall_session_';
+const CURRENT_SESSION_KEY = `${SESSION_KEY_PREFIX}${CURRENT_VERSION}`;
+
 export default function setupSession(global) {
   // The name of the property encodes the session/node compatibilty version
-  if (!global._heimdall_session_3) {
-    global._heimdall_session_3 = new Session();
+  if (!global[CURRENT_SESSION_KEY]) {
+    global[CURRENT_SESSION_KEY] = new Session();
   }
 
-  for (let i = 1; i < 3; i++) {
-    let key = `_heimdall_session_${i}`;
+  for (let i = 1; i < CURRENT_VERSION; i++) {
+    let key = `${SESSION_KEY_PREFIX}${i}`;
     if (global[key]) {
-      warn(`Using HeimdallSession Version 3, but a global session for HeimdallSession version ${i} also exists!.`);
+      warn(`Using HeimdallSession Version ${CURRENT_VERSION}, but a global session for HeimdallSession version ${i} also exists!.`);
     }
   }
 }
