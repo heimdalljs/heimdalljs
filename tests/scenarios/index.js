0caa3150fdca7c0e0bcdcd24575b3d617537fae1
Chris Thoburn <cthoburn@linkedin.com>
2017-02-26 16:19:25 -0800

    adds scenario based test coverage for using Array instead of Uint32Array

---
+++ tests/scenarios/index.js
@@ -0,0 +1,15 @@
+import doubleArray from '../mock/has-typed-arrays';
+
+export default {
+
+  default: {
+    setup() {}
+  },
+
+  'array-fallback': {
+    setup() {
+      doubleArray(false);
+    }
+  }
+
+};
