eaaf27f6b320cbb2fa7259b5166d05accc509724
Marc Lynch <lynchmarc2@gmail.com>
2018-08-29 15:32:47 -0700

    lerna, yarn packages init

--- packages/heimdalljs/node.config.js
+++ packages/heimdalljs/node.config.js
@@ -4,32 +4,27 @@ import replace from 'rollup-plugin-replace';
 import typescript from 'rollup-plugin-typescript';
 
 export default {
-  input: 'tests/index.ts',
+  input: 'src/runtime/node/index.ts',
   output: [{
-      name: 'heimdall-js',
-      file: 'dist/tests/bundle.cjs.js',
+      name: 'heimdalljs',
+      file: 'dist/heimdalljs.cjs.js',
       format: 'cjs'
     }, {
-      name: 'heimdall-js',
-      file: 'dist/tests/bundle.umd.js',
-      format: 'umd'
-    }, {
-      name: 'heimdall-js',
-      file: 'dist/tests/bundle.es.js',
-      format: 'es'  
+      name: 'heimdalljs',
+      file: 'dist/heimdalljs.es.js',
+      format: 'es'
   }],
   plugins: [
     typescript({
       include: [
-        'src/**/*',
-        'tests/**/*'
+        'src/**/*'
       ],
       exclude: [
-        'node_modules/**'
+        '../../node_modules/**'
       ]
     }),
     nodeResolve({ jsnext: true, main: true }),
-    commonjs({ include: 'node_modules/**', ignoreGlobal: true }),
+    commonjs({ include: '../../node_modules/**', ignoreGlobal: true }),
     replace({
       VERSION_STRING_PLACEHOLDER: require('./package').version
     }),
